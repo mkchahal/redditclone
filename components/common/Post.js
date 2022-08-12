76e0e23e3a49b0dca0ed9d414ab5e199cd2d6bbc
Mandeep <mandeep.windsor@gmail.com>
2022-08-11 23:18:29 -0700

    Created Info  component and styled it.

--- components/common/Post.js
+++ components/common/Post.js
@@ -1,28 +1,30 @@
 import Vote from '../feed/Vote';
 import Actions from '../feed/Actions';
+import Info from '../feed/Info';
 
 const style = {
     post: 'flex flex-col space-y-1 cursor-pointer',
     wrapper: 'flex space-x-3 rounded bg-[#1a1a1a]/80 p-2 border border-[#343536]',
-    postTitle: 'text-lg font-medium text-[#D7DADC]', 
-    postContent: 'text-sm font-light text-[#D7DADC]/80', 
+    postTitle: 'text-lg font-medium text-[#D7DADC]',
+    postContent: 'text-sm font-light text-[#D7DADC]/80',
 }
 
 const Post = () => {
-  return (
-    <div className={style.wrapper}>
-        <Vote />
-        <div className={style.post}>
-          <h1 className={style.postTitle}>Why puppies are so cute? 💌</h1>
-          <p className={style.postContent}>
-            I always go awww whenever I see some puppies no matter what breed they
-            are. Why so? Why do we love them and feel fuzzy? There could be some
-            science behind it. 🤔
-          </p>
-          <Actions/>
+    return (
+        <div className={style.wrapper}>
+            <Vote />
+            <div className={style.post}>
+                <Info />
+                <h1 className={style.postTitle}>Why puppies are so cute? 💌</h1>
+                <p className={style.postContent}>
+                    I always go awww whenever I see some puppies no matter what breed they
+                    are. Why so? Why do we love them and feel fuzzy? There could be some
+                    science behind it. 🤔
+                </p>
+                <Actions />
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Post;
---
+++ components/feed/Info.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+const style = {
+    profilePic: "h-4 w-4 rounded-full",
+    wrapper: "flex items-center space-x-1 text-xs text-[#818384]",
+    profilePicContainer: "flex items-center space-x-1",
+    tag: "cursor-pointer text-xs font-semibold text-[#D7DADC] hover:underline", 
+    postedBy: "flex items-center space-x-1"
+}
+
+const Info = () => {
+    return (
+        <div className={style.wrapper}>
+            <div className={style.profilePicContainer}>
+                <Image
+                    className="rounded-full"
+                    src="https://images.unsplash.com/photo-1596797882870-8c33deeac224"
+                    height="16"
+                    width="16"
+                    alt="Profile" />
+            </div>
+
+            <div className={style.tag}>r/cutepuppies</div>
+            <div>•</div>
+
+            <div className={style.postedBy}>
+                <span>Posted by Mandeep K Chahal</span>
+                <span>•</span>
+                <span>Aug 11</span>
+            </div>
+        </div>
+    )
+}
+
+export default Info;
