0805c8d592e92237442fcbe80111965979c1df65
Mandeep <mandeep.windsor@gmail.com>
2022-08-14 22:16:58 -0700

    Added functionality to Sign In button and connexted Google 0Auth with supabase.

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import reddit from "../assets/images/reddit-logo.webp";
 import google from "../assets/icons/googleIcon.png";
+import { supabase } from "../services/supabaseClient";
+import Loading from "../components/Loading";
+import { useEffect, useState } from "react";
 
 const style = {
   wrapper: "flex min-h-screen flex-col items-center justify-center gap-y-14",
@@ -9,10 +12,21 @@ const style = {
 };
 
 const Login = () => {
+
+  const signInWithGoogle = async () => {
+    try {
+      await supabase.auth.signIn({
+        provider: 'google'
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className={style.wrapper}>
       <Image src={reddit} width="300" height="300" alt="reddit" />
-      <button className={style.loginBtn}>
+      <button onClick={signInWithGoogle} className={style.loginBtn}>
         <Image src={google} width="25" height="25" alt="google" />
         <span className={style.loginBtnText}>Sign In with Google</span>
       </button>
